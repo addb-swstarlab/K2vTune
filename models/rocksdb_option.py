6135cc7c619cf9459cfdbeee9fac2dba74d72fa5
dkdl012 <dkdl031@naver.com>
2023-05-02 21:25:30 +0900

    add default option

--- models/rocksdb_option.py
+++ models/rocksdb_option.py
@@ -54,6 +54,29 @@ universal_compaction_option = {
     "universal_compression_size_percent": []
 }
 
+def_option = {'max_background_compactions': 1,
+             'max_background_flushes': 1,
+             'write_buffer_size': 1048576,
+             'max_write_buffer_number': 2,
+             'min_write_buffer_number_to_merge': 1,
+             'compaction_pri': 0,
+             'compaction_style': 0,
+             'level0_file_num_compaction_trigger': 4,
+             'level0_slowdown_writes_trigger': 20,
+             'level0_stop_writes_trigger': 36,
+             'compression_type': 'snappy',
+             'bloom_locality': 0,
+             'open_files': -1,
+             'block_size': 4096,
+             'cache_index_and_filter_blocks': 0,
+             'max_bytes_for_level_base': 4194304,
+             'max_bytes_for_level_multiplier': 10,
+             'target_file_size_base': 1048576,
+             'target_file_size_multiplier': 1,
+             'num_levels': 7,
+             'memtable_bloom_size_ratio': 0,
+             'compression_ratio': 0.5}
+
 def make_random_option():
     option_list = ""
     option_dict = {}
