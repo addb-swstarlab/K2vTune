28e9fe735283dd7d62de7c6043440c9124b4ee9e
dkdl012 <31770302+dkdl012@users.noreply.github.com>
2023-10-17 21:54:54 +0900

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -3,9 +3,3 @@
 if [ $# = 4 ] ; then
     python main.py --target $1 --tf --mode $2 --eval --model_path $3 --hidden_size 128 --generation 100 --optimization $4
 fi
-
-
-# if [ $# = 3 ] ; then
-#     python main.py --target $1 --tf --mode $2 --eval --model_path $3 --hidden_size 128 --generation 100 \
-#     --ex_weight 1 --ex_weight 1 --ex_weight 1 --ex_weight 1 --step
-# fi
